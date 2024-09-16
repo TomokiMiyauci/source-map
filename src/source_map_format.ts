7a933cfa50b355fa39c3780c225c6ccd96474b69
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-17 02:47:00 +0900

    feat(source_map_format): add  algorithm and struct

--- src/source_map_format.ts
+++ src/source_map_format.ts
@@ -1,3 +1,5 @@
+import { List, type Map } from "@miyauci/infra";
+
 /**
  * [Source Map](https://tc39.es/source-map/#source-map-format)
  */
@@ -63,4 +65,225 @@ export interface SourceMap {
    * [Source Map](https://tc39.es/source-map/#ignorelist)
    */
   ignoreList?: number[];
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#decoded-source-map)
+ */
+export class DecodedSourceMap {
+  /**
+   * [Source Map](https://tc39.es/source-map/#decoded-source-map-file)
+   */
+  file!: string | null;
+
+  /**
+   * [Source Map](https://tc39.es/source-map/#decoded-source-map-sources)
+   */
+  sources!: List<DecodedSource>;
+
+  /**
+   * [Source Map](https://tc39.es/source-map/#decoded-source-map-mappings)
+   */
+  mappings!: List<unknown>;
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#decoded-source)
+ */
+export interface DecodedSource {
+  /**
+   * [Source Map](https://tc39.es/source-map/#decoded-source-url)
+   */
+  url: URL | null;
+
+  /**
+   * [Source Map](https://tc39.es/source-map/#decoded-source-content)
+   */
+  content: string | null;
+
+  /**
+   * [Source Map](https://tc39.es/source-map/#decoded-source-ignored)
+   */
+  ignored: boolean;
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#optionally-get-a-string)
+ */
+export function optionallyGetString(
+  key: string,
+  jsonMap: Map<string, unknown>,
+): string | null {
+  // 1. If jsonMap[key] does not exist, return null.
+  if (!jsonMap.exists(key)) return null;
+
+  const value = jsonMap.get(key);
+  // 2. If jsonMap[key] is not a string, optionally report an error and return null.
+  if (typeof value !== "string") {
+    optionallyReportError();
+    return null;
+  }
+
+  // 3. Return jsonMap[key].
+  return value;
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#optionally-get-a-list-of-strings)
+ */
+export function optionallyGetListString(
+  key: string,
+  jsonMap: Map<string, unknown>,
+): List<string> {
+  // 1. If jsonMap[key] does not exist, return a new empty list.
+  if (!jsonMap.exists(key)) return new List();
+
+  const items = jsonMap.get(key)!;
+  // 2. If jsonMap[key] is not a list, optionally report an error and return a new empty list.
+  if (!(items instanceof List)) {
+    optionallyReportError();
+    return new List();
+  }
+
+  // 3. Let list be a new empty list.
+  const list = new List<string>();
+
+  // 4. For each jsonItem of jsonMap[key]:
+  for (const jsonItem of items) {
+    // 1. If jsonItem is a string, append it to list.
+    if (typeof jsonItem === "string") list.append(jsonItem);
+    // 2. Else, optionally report an error and append "" to list.
+    else list.append("");
+  }
+
+  // 5. Return list.
+  return list;
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#optionally-get-a-list-of-optional-strings)
+ */
+export function optionallyGetListOfOptionallyStrings(
+  key: string,
+  jsonMap: Map<string, unknown>,
+): List<string | null> {
+  // 1. If jsonMap[key] does not exist, return a new empty list.
+  if (!jsonMap.exists(key)) return new List();
+
+  const items = jsonMap.get(key)!;
+  // 2. If jsonMap[key] is not a list, optionally report an error and return a new empty list.
+  if (!(items instanceof List)) {
+    optionallyReportError();
+    return new List();
+  }
+
+  // 3. Let list be a new empty list.
+  const list = new List<string | null>();
+
+  // 4. For each jsonItem of jsonMap[key]:
+  for (const jsonItem of items) {
+    // 1. If jsonItem is a string, append it to list.
+    if (typeof jsonItem === "string") list.append(jsonItem);
+    // 2. Else,
+    else {
+      // 1. If jsonItem is not null, optionally report an error.
+      if (jsonItem !== null) optionallyReportError();
+
+      // 2. Append null to list.
+      list.append(null);
+    }
+  }
+
+  // 5. Return list.
+  return list;
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#optionally-get-a-list-of-array-indexes)
+ */
+export function optionallyGetListArrayIndex(
+  key: string,
+  jsonMap: Map<string, unknown>,
+): List<number | null> {
+  // If jsonMap[key] does not exist, return a new empty list.
+  if (!jsonMap.exists(key)) return new List();
+
+  const items = jsonMap.get(key)!;
+  // If jsonMap[key] is not a list, optionally report an error and return a new empty list.
+  if (!(items instanceof List)) {
+    optionallyReportError();
+    return new List();
+  }
+
+  // Let list be a new empty list.
+  const list = new List<number | null>();
+
+  // For each jsonItem of jsonMap[key]:
+  for (const jsonItem of items) {
+    // If jsonItem is a non-negative integer number, append it to list.
+    if (0 <= jsonItem && Number.isInteger(jsonItem)) list.append(jsonItem);
+    // Else,
+    else {
+      // If jsonItem is not null, optionally report an error.
+      if (jsonItem !== null) optionallyReportError();
+
+      // Append null to list.
+      list.append(null);
+    }
+  }
+
+  // Return list.
+  return list;
+}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#optionally-report-an-error)
+ */
+export function optionallyReportError() {}
+
+/**
+ * [Source Map](https://tc39.es/source-map/#decoded-mapping)
+ */
+export interface DecodedMapping {
+  /**
+   * A non-negative integer.
+   *
+   * [Source Map](https://tc39.es/source-map/#decoded-mapping-generatedline)
+   */
+  generatedLine: number;
+
+  /**
+   * A non-negative integer.
+   *
+   * [Source Map](https://tc39.es/source-map/#decoded-mapping-generatedcolumn)
+   */
+  generatedColumn: number;
+
+  /**
+   * A decoded source or null.
+   *
+   * [Source Map](https://tc39.es/source-map/#decoded-mapping-originalsource)
+   */
+  originalSource: DecodedSource | null;
+
+  /**
+   * A non-negative integer or null.
+   *
+   * [Source Map](https://tc39.es/source-map/#decoded-mapping-originalline)
+   */
+  originalLine: number | null;
+
+  /**
+   * A non-negative integer or null.
+   *
+   * [Source Map](https://tc39.es/source-map/#decoded-mapping-originalcolumn)
+   */
+  originalColumn: number | null;
+
+  /**
+   * A string or null.
+   *
+   * [Source Map](https://tc39.es/source-map/#decoded-mapping-name)
+   */
+  name: string | null;
 }
