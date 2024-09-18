b83ecb0dfe723247642fee9a21adf0501c67f7ff
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-18 17:54:05 +0900

    feat(mod): export releated modules

--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,17 @@
-export { type SourceMap } from "./source_map_format.ts";
+export {
+  decodeBase64VLQ,
+  type DecodedMapping,
+  type DecodedSource,
+  type DecodedSourceMap,
+  decodeSourceMap,
+  decodeSourceMapFromJSONString,
+  decodeSourceMapMappings,
+  decodeSourceMapSources,
+  optionallyGetArrayIndexList,
+  optionallyGetOptionallyStringList,
+  optionallyGetString,
+  optionallyGetStringList,
+  optionallyReportError,
+  type SourceMap,
+} from "./source_map_format.ts";
 export { type IndexMap, type Section } from "./index_map.ts";
