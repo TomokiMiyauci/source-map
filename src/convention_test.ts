e6287a95de914a459b6aee6255cf6538b5975d08
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-23 21:47:34 +0900

    test(convention): add test case

--- src/convention_test.ts
+++ src/convention_test.ts
@@ -1,7 +1,8 @@
 import { expect } from "@std/expect";
-import { describe, it } from "@std/testing/bdd";
+import { afterEach, beforeEach, describe, it } from "@std/testing/bdd";
 import {
   extractSourceMapURLWithoutParsing,
+  fetchSourceMap,
   matchSourceMapURLInComment,
 } from "./convention.ts";
 
@@ -29,5 +30,65 @@ describe("matchSourceMapURLInComment", () => {
     expect(matchSourceMapURLInComment(`@ sourceMappingURL=foo.js`)).toBe(
       "foo.js",
     );
+  });
+});
+
+describe("fetchSourceMap", () => {
+  it("should return source map with file url", async () => {
+    const url = new URL(
+      import.meta.resolve("../tests/fixtures/sourcemap.js.map"),
+    );
+
+    const sourceMap = await fetchSourceMap(url);
+
+    expect(sourceMap).toEqual({
+      version: 3,
+      file: "helloworld.js",
+      mappings: "AAAA;AAAA,EAAA,OAAO,CAAC,GAAR,CAAY,aAAZ,CAAA,CAAA;AAAA",
+      names: [],
+      sources: [
+        "helloworld.coffee",
+      ],
+    });
+  });
+
+  describe("HTTP related", () => {
+    interface Context {
+      url: URL;
+      server: Deno.HttpServer<Deno.NetAddr>;
+    }
+
+    const url = new URL(
+      import.meta.resolve("../tests/fixtures/escaped_sourcemap.txt"),
+    );
+
+    beforeEach<Context>(async function () {
+      const port = 8000;
+      const sourcemap = await Deno.readTextFile(url);
+
+      this.server = Deno.serve({ port, onListen() {} }, () => {
+        return new Response(sourcemap);
+      });
+
+      this.url = new URL(`http://localhost:${port}`);
+    });
+
+    afterEach<Context>(async function () {
+      await this.server.shutdown();
+    });
+
+    it<Context>("should return source map with http url", async function () {
+      const sourceMap = await fetchSourceMap(this.url);
+
+      expect(sourceMap).toEqual({
+        version: 3,
+        file: "helloworld.js",
+        mappings: "AAAA;AAAA,EAAA,OAAO,CAAC,GAAR,CAAY,aAAZ,CAAA,CAAA;AAAA",
+        names: [],
+        sources: [
+          "helloworld.coffee",
+        ],
+      });
+    });
   });
 });
